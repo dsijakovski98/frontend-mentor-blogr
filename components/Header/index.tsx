340f6fdc393ea53efc0e950ac835c1227d2af97c
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2023-11-14 00:37:33 +0100

    Initial header markup + simple styles

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -10,27 +10,37 @@ const Header = () => {
     <header>
       <Container>
         <nav>
-          <ul>
-            <li>
-              <h1>
-                <Link href='/'>Blogr</Link>
-              </h1>
-            </li>
+          <h1>
+            <Link href='/'>Blogr</Link>
+          </h1>
 
+          <ul className='page-links'>
             {LINKS.map((link) => (
               <li key={link.label}>
                 <Link href='#'>{link.label}</Link>
               </li>
             ))}
+          </ul>
 
+          <ul>
             <li>
               <Link href='#'>Login</Link>
             </li>
             <li>
-              <Link href='#'>Login</Link>
+              <Link href='#'>Sign Up</Link>
             </li>
           </ul>
         </nav>
+
+        <div className='cta'>
+          <h2>A modern publishing platform</h2>
+          <p>Grow your audience and build your online brand</p>
+
+          <div className='actions'>
+            <Link href='#'>Start for Free</Link>
+            <Link href='#'>Learn More</Link>
+          </div>
+        </div>
       </Container>
     </header>
   )
